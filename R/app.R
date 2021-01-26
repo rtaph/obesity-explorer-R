dde933442914d4fcf7eb691f85acadb5263adebf
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-01-25 22:25:37 -0800

    Add skeleton of app and source files

---
+++ R/app.R
@@ -0,0 +1,176 @@
+# Obesity-explorer application
+
+# Load libraries
+library(dash)
+library(dashHtmlComponents)
+library(dashCoreComponents)
+library(dashBootstrapComponents)
+library(plotly)
+library(tidyverse)
+
+# Load custom functions
+source(here::here("R", "helper.R"))
+source(here::here("R", "plots.R"))
+source(here::here("R", "text.R"))
+
+
+# Read-in the data
+datapath <- here::here("data", "processed", "obesity-combo.csv")
+ob <- readr::read_csv(datapath) %>%
+  filter(region != "Aggregates")
+
+
+# Load CSS Styles
+css <- custom_css()
+
+# Define app layout
+app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
+app$layout(
+  dbcContainer(
+    list(
+      htmlH1("Obesity Dashboard"),
+      dccMarkdown(header),
+      dbcRow(
+        list(
+          dbcCol( # INPUT CONTROLS
+            id = "options-panel",
+            style = css$box,
+            md = 4,
+            list(
+              htmlBr(),
+              dbcLabel("Filter Sex:"),
+              dccRadioItems(
+                id = "input_sex",
+                options = list(
+                  list(label = "Male", value = "Male"),
+                  list(label = "Female", value = "Female"),
+                  list(label = "Both", value = "Both")
+                ),
+                css$dd,
+                labelStyle = list("display" = "inline-block"),
+                inputStyle = list("margin-left" = "20px"),
+                value = "Both"
+              ),
+              htmlBr(),
+              dbcLabel("Filter Year:"),
+              dccSlider(
+                id = "input_year",
+                min = 1975,
+                max = 2016,
+                step = 1,
+                value = 2016,
+                included = FALSE
+                # marks = year_range
+              ),
+              htmlBr(),
+              dbcLabel("Filter Region:"),
+              dccDropdown(
+                id = "input_region",
+                options = map(unique(ob$region), ~ list(label = ., value = .)),
+                value = unique(ob$region),
+                clearable = FALSE,
+                style = css$dd,
+                multi = TRUE
+              ),
+              htmlBr(),
+              dbcLabel("Filter Income Group:"),
+              dccDropdown(
+                id = "input_income",
+                options = map(unique(ob$income), ~ list(label = ., value = .)),
+                value = unique(ob$income),
+                style = css$dd,
+                clearable = FALSE,
+                multi = TRUE
+              ),
+              htmlBr(),
+              dbcLabel("Select Secondary Variable: "),
+              dccDropdown(
+                id = "input_regressor",
+                options = list(
+                  list(label = "Smoking Rate", value = "smoke"),
+                  list(label = "Primary Education Completion Rate", value = "primedu"),
+                  list(label = "Unemployment Rate", value = "unemployed")
+                ),
+                value = "primedu",
+                clearable = FALSE,
+                style = css$dd,
+                multi = FALSE
+              ),
+              htmlBr(),
+              dbcLabel("Select Grouping Variable: "),
+              dccDropdown(
+                id = "input_grouper",
+                options = list(
+                  list(label = "Income group", value = "income"),
+                  list(label = "Sex", value = "sex"),
+                  list(label = "Region", value = "region"),
+                  list(label = "No grouping", value = "none")
+                ),
+                value = "none",
+                style = css$dd,
+                clearable = FALSE,
+                multi = FALSE
+              ),
+              htmlBr(),
+              dbcLabel("Highlight Countries:"),
+              dccDropdown(
+                id = "input_highlight_country",
+                options = map(unique(ob$country), ~ list(label = ., value = .)),
+                value = "Canada",
+                clearable = TRUE,
+                searchable = TRUE,
+                multi = TRUE
+              ),
+              htmlBr(),
+              dbcLabel("Select Year Range:"),
+              dccRangeSlider(
+                id = "input_year_range",
+                min = 1975,
+                max = 2016,
+                step = 1,
+                value = c(1975, 2016)
+                # marks = year_range
+              ),
+              htmlHr(),
+              dccMarkdown(footer, style = css$sources)
+            )
+          ),
+          dbcCol( # PLOTTING PANEL
+            list(
+              dbcRow(
+                # TIME SERIES PLOT
+                dccGraph(id = "ts_plot")
+              ),
+              dbcRow(
+                # SCATTER PLOT
+                dccGraph(id = "scatter_plot")
+              ),
+              dbcRow(
+                # TIME SERIES PLOT
+                dccGraph(id = "chloropleth_plot")
+              ),
+              dbcRow(
+                # SCATTER PLOT
+                dccGraph(id = "bar_plot")
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+)
+
+app$callback(
+  output("bar_plot", "figure"),
+  list(
+    input("input_region", "value"),
+    input("input_year", "value"),
+    input("input_income", "value"),
+    input("input_sex", "value")
+  ),
+  make_bar_plot
+)
+
+
+app$run_server(debug = TRUE)
--- R/helper.R
+++ R/helper.R
@@ -1,48 +1,100 @@
 # Helper functions
 
-#' Safely calculate a rate
+#' Safely Calculate a Rate
 #'
-#' @param x A continuous variable acting as a numerator.
-#' @param y A continuous variable acting as a denominator
+#' Calculates sum(x) / sum(y) for values for non-missing values in (x, y).
 #'
-#' @return A vector of rates
+#' @param x A numeric variable acting as a numerator.
+#' @param y A numeric variable acting as a denominator.
+#'
+#' @return A scalar rate.
 #' @export
 #'
 #' @examples
-#' rate(c(1, 2, NA, 4), c(5, NA, 6, 7))
-#' 
+#' x <- c(1, 2, NA, 4)
+#' y <- c(5, NA, 6, 7)
+#' rate(x, y)
 rate <- function(x, y) {
-  i <- !(is.na(x) & is.na(y))
-  sum(x[i], na.rm=TRUE) / sum(y[i], na.rm=TRUE)
+  i <- !(is.na(x) | is.na(y))
+  sum(x[i]) / sum(y[i])
 }
 
-#' Generate a tidy dataframe that includes rates for selected variables
+
+#' Generate a Rate Data for Plotting
 #'
-#' @param grp A string of categorical grouping variable names
-#' @param valuevars A string of continuous variables for which a rate will be
-#'   calculated relative to population
-#' @param query A list of filters, where the list element name is the name of
-#'   the variable and the element value is a string of values who's membership
-#'   should be included
-#' @param data A dataframe for which we are filtering and calculating rate in
+#' @param grp A character string of categorical grouping variable names.
+#' @param fltr A named list of filters, where each list element name is the
+#'   name of the variable and the element value is a string of values whose
+#'   membership is to be evaluated (see examples).
+#' @param vals A character string of continuous variables for which a rate will be
+#'   calculated relative to population. Defaults to "obese."
+#' @param data The obesity dataframe.
 #'
-#' @return A grouped dataframe
+#' @return A dataframe with rates calculated for each value in vals.
 #' @export
 #' @import dplyr rlang
 #'
 #' @examples
 #' ob <- readr::read_csv(here::here("data", "processed", "obesity-combo.csv"))
 #' grp <- c("sex", "country")
-#' valuevars <- c("obese", "unemployed")
-#' query <- list("region" = c("South Asia", "Europe & Central Asia"),
-#'               "year" = c(2014, 2015, 2016))
-#' make_rate_data(grp, valuevars, query, ob)
-make_rate_data <- function(grp, valuevars, query, data=ob) {
-  data %>% 
-    group_by(!!!syms(grp)) %>% 
-    filter(region %in% query$region, 
-           year %in% query$year,
-           income %in% query$income,
-           country %in% query$country) %>% 
-    summarize(across(c(!!!syms(valuevars)), list(rate = ~rate(., pop))))
+#' vals <- c("obese", "unemployed")
+#' fltr <- list(
+#'   region = c("South Asia", "Europe & Central Asia"),
+#'   year = c(2014, 2015, 2016)
+#' )
+#' make_rate_data(grp, query, vals)
+make_rate_data <- function(grp, fltr, vals = "obese", data = ob) {
+  data %>%
+    group_by(!!!syms(grp)) %>%
+    filter(across(all_of(names(fltr)), ~ . %in% fltr[[cur_column()]])) %>%
+    summarise(across(all_of(vals), list(rate = ~ rate(., pop))),
+      .groups = "drop"
+    )
+}
+
+
+#' Remap the Sex Input Variable
+#'
+#' Remaps to c("Male", "Female") if user selects "Both"
+#'
+#' @param x A scalar string indicating the radio button selection.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' remap_sex("Male")
+#' remap_sex("Both")
+remap_sex <- function(x) {
+  stopifnot(length(x) == 1 & x %in% c("Male", "Female", "Both"))
+  if (x == "Both") {
+    return(c("Female", "Male"))
+  } else {
+    return(x)
+  }
+}
+
+
+#' List of Custom CSS Specs
+#'
+#' @return A named list of lists containing CSS specifications
+#' @export
+custom_css <- function() {
+  css <- list()
+  
+  # Input parameter box
+  css$box <- list(
+    "border" = "1px solid #d3d3d3",
+    "border-radius" = "10px",
+    "background-color" = "rgba(220, 220, 220, 0.5)"
+  )
+  
+  # Drop-down choices
+  css$dd <- list("font-size" = "smaller")
+  
+  # Footnote text
+  css$sources <- list("font-size" = "xx-small")
+  
+  # Return CSS
+  css
 }
