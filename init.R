a88b59592b0f3ace8849c2900d43bfd598204add
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-02-05 17:22:55 -0800

    Memoize callbacks

--- init.R
+++ init.R
@@ -2,14 +2,16 @@
 # contains placeholders which are inserted by the compile script
 # NOTE: this script is executed in the chroot context; check paths!
 
-r <- getOption('repos')
-r['CRAN'] <- 'https://mran.microsoft.com/snapshot/2021-01-30'
-options(repos=r)
+r <- getOption("repos")
+r["CRAN"] <- "https://mran.microsoft.com/snapshot/2021-01-30"
+options(repos = r)
 
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes',
-                   'tidyverse','plotly', 'lintr', 'spelling',
-                   'checkmate', 'testthat', 'devtools', 'dplyr'))
-remotes::install_github('facultyai/dash-bootstrap-components@r-release')
+install.packages(c(
+  "dash", "readr", "here", "ggthemes", "remotes",
+  "tidyverse", "plotly", "lintr", "spelling", "memoise",
+  "checkmate", "testthat", "devtools", "dplyr"
+))
+remotes::install_github("facultyai/dash-bootstrap-components@r-release")
